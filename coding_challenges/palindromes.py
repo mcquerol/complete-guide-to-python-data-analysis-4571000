9141668103cf75b52f0c68332ea1a5d75b3f0f1d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-12 13:17:16 +0100

    get rid of the continue and check both conditions directly

--- coding_challenges/palindromes.py
+++ coding_challenges/palindromes.py
@@ -4,15 +4,13 @@
 show_expected_result = True
 show_hints = True
 
+import itertools
+
 def palindromes(anagrams_by_signature):
     # Your code goes here.
     palindromesList = []
     for group in anagrams_by_signature.values():
-        for word in group:
-            if len(word) < 7:
-                continue
-            else:
-                rev = word[::-1]
-                if rev in group and rev != word:
-                    palindromesList.append({word, rev})
+        for word1, word2 in itertools.combinations(group, 2):
+            if word1 == word2[::-1] and len(word1) > 6:
+                palindromesList.append({word1, word2})
     return palindromesList

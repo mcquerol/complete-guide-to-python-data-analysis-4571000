01df806ab64c9e60b1f30a56472e078f7b4196db
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-12 13:04:37 +0100

    add palindrome code to complete

---
+++ coding_challenges/palindromes.py
@@ -0,0 +1,11 @@
+# Python code below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+
+def palindromes(anagrams_by_signature):
+    # Your code goes here.
+
+    return []

3060fffb41d949dbb9dcffab3f1dbb7b6f8f6fa3
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-12 13:10:59 +0100

    add palindrome sets to new list

--- coding_challenges/palindromes.py
+++ coding_challenges/palindromes.py
@@ -1,11 +1,18 @@
 # Python code below
 # Use print("messages...") to debug your solution.
 
-show_expected_result = False
-show_hints = False
-
+show_expected_result = True
+show_hints = True
 
 def palindromes(anagrams_by_signature):
     # Your code goes here.
-
-    return []
+    palindromesList = []
+    for group in anagrams_by_signature.values():
+        for word in group:
+            if len(word) < 7:
+                continue
+            else:
+                rev = word[::-1]
+                if rev in group and rev != word:
+                    palindromesList.append({word, rev})
+    return palindromesList

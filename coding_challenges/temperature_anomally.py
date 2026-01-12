6c6ac2d546c090e726de178a3b4beb08ccba73d6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-12 15:30:35 +0100

    add code for temperature anomaly exercise

---
+++ coding_challenges/temperature_anomally.py
@@ -0,0 +1,13 @@
+# Python code below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+import numpy as np
+
+
+def anomaly(allyears):
+    # Your code goes here.
+
+    return np.array([], dtype=np.float64)

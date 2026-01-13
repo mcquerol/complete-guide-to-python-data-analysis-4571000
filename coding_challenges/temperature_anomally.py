45ff742ac695a131208a6ccd3f96b7b23d722457
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-13 09:42:11 +0100

    add anomaly calculation from reference np array

--- coding_challenges/temperature_anomally.py
+++ coding_challenges/temperature_anomally.py
@@ -9,5 +9,8 @@ import numpy as np
 
 def anomaly(allyears):
     # Your code goes here.
+    refmean =  0.5 * (np.mean(allyears[65:76]['TMIN']) + np.mean(allyears[65:76]['TMAX']))
+    anomaly = 0.5 * (np.mean(allyears['TMIN'], axis=1) + np.mean(allyears['TMAX'], axis=1) - refmean)
 
-    return np.array([], dtype=np.float64)
+    return np.round(anomaly, 2)
+

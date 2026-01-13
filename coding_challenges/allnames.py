1cf1b68af7afbb6f7304e932608aeed6ac10569e
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-13 11:40:59 +0100

    create boys df

---
+++ coding_challenges/allnames.py
@@ -0,0 +1,14 @@
+# Python code below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+import pandas as pd
+
+def unisex(allnames):
+    # Your code goes here.
+    print(allnames)
+    boys = allnames.loc[allnames['sex'] == 'M']
+    print(boys)
+

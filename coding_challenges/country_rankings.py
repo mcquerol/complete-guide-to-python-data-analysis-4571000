ace37efb8882d411ee458231a3aae0a60d5f935e
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-01-13 16:46:45 +0100

    add skeleton for country rankings exercise

---
+++ coding_challenges/country_rankings.py
@@ -0,0 +1,10 @@
+# Python code below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+import pandas as pd
+
+def rankcountries(medals):
+    return pd.DataFrame([])
